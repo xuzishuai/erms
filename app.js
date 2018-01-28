fac9669322dc4503d9edb448c3a3d0bedc6e83d1
zishuai <825197298@qq.com>
2018-01-28 16:24:10 +0800

    增加metronic和jQuery

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
 
 const index = require('./routes/index');
-const users = require('./routes/users');
+const user = require('./routes/user');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,10 +23,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'erms',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000*3600*24 }
+}));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', index);
-app.use('/users', users);
+app.use('/user', user);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +50,7 @@ app.use(function(err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render('error', {err: res.locals.message});
 });
 
 module.exports = app;
