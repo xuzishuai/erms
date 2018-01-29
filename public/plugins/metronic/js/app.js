1ac41d1add055409996b4e16fc91e80bd4ab709c
徐子帅 <825197298@qq.com>
2018-01-29 17:49:25 +0800

    为页面增加自适应高度js

--- public/plugins/metronic/js/app.js
+++ public/plugins/metronic/js/app.js
@@ -130,7 +130,19 @@ var App = function () {
 
     //* BEGIN:CORE HANDLERS *//
     // this function handles responsive layout on screen size resize or mobile device rotate.
-  
+    var getViewPort = function() {
+        var e = window,
+            a = 'inner';
+        if (!('innerWidth' in window)) {
+            a = 'client';
+            e = document.documentElement || document.body;
+        }
+
+        return {
+            width: e[a + 'Width'],
+            height: e[a + 'Height']
+        };
+    }
     var handleSidebarAndContentHeight = function () {
         var content = $('.page-content');
         var sidebar = $('.page-sidebar');
@@ -145,12 +157,24 @@ var App = function () {
         } else {
             if (body.hasClass('page-sidebar-fixed')) {
                 height = _calculateFixedSidebarViewportHeight();
+                if (body.hasClass('page-footer-fixed') === false) {
+                    height = height - $('.page-footer').outerHeight();
+                }
             } else {
-                height = sidebar.height() + 20;
+                var headerHeight = $('.header').outerHeight();
+                var footerHeight = $('.footer').outerHeight();
+
+                if (getViewPort().width < 992) {
+                    height = getViewPort().height - headerHeight - footerHeight;
+                } else {
+                    height = sidebar.height() + 20;
+                }
+
+                if ((height + headerHeight + footerHeight) <= getViewPort().height) {
+                    height = getViewPort().height - headerHeight - footerHeight;
+                }
             }
-            if (height >= content.height()) {
-                content.attr('style', 'min-height:' + height + 'px !important');
-            } 
+            content.attr('style', 'min-height:' + height + 'px');
         }          
     }
 
