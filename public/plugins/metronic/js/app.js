25b815393257e917dd0a3482a05a5eb72f78ac1c
zishuai <825197298@qq.com>
2018-03-12 22:49:17 +0800

    select2增加x按钮来清空选项

--- public/plugins/metronic/js/app.js
+++ public/plugins/metronic/js/app.js
@@ -738,6 +738,7 @@ var App = function () {
     var handleSelect2 = function() {
         if ($().select2) {
             $('.select2').select2({
+                placeholder: "",
                 allowClear: true,
                 formatNoMatches: function () { return "没有找到匹配项"; },
                 formatLoadMore: function () { return "加载结果中…"; },
